92dcda93d41417dbe4aa82f1d2e7458c646dad47
Matteo Angelini <matteo.angelini@outlook.com>
2022-05-13 10:10:25 +0200

    Frontend base app

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -1,20 +1,24 @@
 module.exports = {
   networks: {
     development: {
-     host: "127.0.0.1",
-     port: 8545,
-     network_id: "*"
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
     },
-    dashboard: {
-    }
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+    dashboard: {},
   },
   compilers: {
     solc: {
       version: "0.8.13",
-    }
+    },
   },
   db: {
     enabled: false,
     host: "127.0.0.1",
-  }
+  },
 };
